d9eca5d11a3df2471f0854e8076dc6e409169923
Shennie <shenniewu@gmail.com>
2023-06-01 16:49:57 +0000

    CPW#0

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import '../styles/App.css';
+import Board from './Board.tsx';
+
+export default function App() {
+  return (
+    < >
+      <h1>2048</h1>
+      <Board />  
+    </>
+  );
+}
---
+++ src/components/Board.tsx
@@ -0,0 +1,12 @@
+import Tile from './Tile.tsx';
+
+export default function Board() {
+  return (
+    <main className='board'>
+      <section className='board__background'>
+      </section>
+      <section className='board__tiles'>
+      </section>
+    </main>
+  );
+}
